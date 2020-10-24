55bef3ad1eadae4436f63a6abd39da91b39d43c7
Panji Brotoisworo <panji.p.broto@gmail.com>
2020-10-25 02:40:49 +0800

    Added image classifier: Boost

---
+++ scripts/TrainImagesClassifier_Boost.py
@@ -0,0 +1,142 @@
+import arcpy
+import os
+import shutil
+import subprocess
+from datetime import datetime
+from cl_tools import generate_command, execute_command
+
+arcpy.AddMessage('\nTrain Images Classifier (Decision Tree)')
+arcpy.AddMessage('Orfeo Toolbox\n')
+
+# Detect workspace, set up initial parameters
+workspace = os.path.dirname(os.path.abspath(__file__))
+log_folder = os.path.join(os.path.dirname(workspace), 'logs')
+scratch_folder = os.path.join(workspace, 'scratch')
+arcpy.env.overwriteOutput = True
+
+# Set logging parameters
+ts = datetime.now()
+ts = ts.strftime("%Y-%m-%d %H:%M:%S")
+ts_file_append = ts.replace('-', '')
+ts_file_append = ts_file_append.replace(':', '')
+ts_file_append = ts_file_append.replace(' ', '_')
+log_file = os.path.join(log_folder, 'TRAINCLASSBOOST' + ts_file_append + '.log')
+
+# Load OTB Dir
+with open(os.path.join(workspace, 'OTBDIR.ini'), 'r') as f:
+    otb_dir = f.read()
+    otb_dir_devenv = os.path.join(otb_dir, 'start_devenv.bat')
+
+if not os.path.exists(otb_dir):
+    raise Exception('OTB files not found!')
+
+# Input vars
+input_images = arcpy.GetParameterAsText(0)
+input_shapefiles = arcpy.GetParameterAsText(1)
+input_xml_stats = arcpy.GetParameterAsText(2)
+input_default_elev = arcpy.GetParameterAsText(3)
+input_max_class_train_sample = arcpy.GetParameterAsText(4)
+input_max_class_valid_sample = arcpy.GetParameterAsText(5)
+input_bound_sample_num_min = arcpy.GetParameterAsText(6)
+input_train_validation_ratio = arcpy.GetParameterAsText(7)
+input_name_discrimination_field = arcpy.GetParameterAsText(8)
+input_train_method = 'boost'
+input_boost_type = arcpy.GetParameterAsText(9)
+input_weak_count = arcpy.GetParameterAsText(10)
+input_weight_trim_rate = arcpy.GetParameterAsText(11)
+input_max_depth_tree = arcpy.GetParameterAsText(12)
+input_ram = arcpy.GetParameterAsText(13)
+out_conf_matrix = arcpy.GetParameterAsText(14)
+out_model = arcpy.GetParameterAsText(15)
+
+# Generate OTB commands
+command_list = []
+
+# State which classifier is being used
+input_classifier = '-classifier boost'
+command_list.append(input_classifier)
+
+# Generate command for input imagery
+otb_input_images = generate_command('-io.il ', True, input_images, True)
+command_list.append(otb_input_images)
+
+# Generate command for input shapefiles
+otb_input_train_shp = generate_command('-io.vd ', True, input_shapefiles, True)
+command_list.append(otb_input_train_shp)
+
+# Generate command for XML Stats
+if len(input_xml_stats) > 0:
+    otb_input_xml_stats = generate_command('-io.imstat ', True, input_xml_stats, False)
+    command_list.append(otb_input_xml_stats)
+
+# Generate command for default elevation
+otb_input_default_elev = generate_command('-elev.default ', False, input_default_elev, False)
+command_list.append(otb_input_default_elev)
+
+# Generate command for max train class sample
+otb_input_max_class_train_sample = generate_command('-sample.mt ', False, input_max_class_train_sample, False)
+command_list.append(otb_input_max_class_train_sample)
+
+# Generate command for max valid class sample
+otb_input_max_class_valid_sample = generate_command('-sample.mv ', False, input_max_class_valid_sample, False)
+command_list.append(otb_input_max_class_valid_sample)
+
+# Generate command for bound sample number by minimum
+otb_input_bound_sample_num_min = generate_command('-sample.bm ', False, input_bound_sample_num_min, False)
+command_list.append(otb_input_bound_sample_num_min)
+
+# Generate command for training and validation sample ratio
+otb_input_train_validation_ratio = generate_command('-sample.vtr ', False, input_train_validation_ratio, False)
+command_list.append(otb_input_train_validation_ratio)
+
+# Generate command for discrimination field
+otb_input_name_discrimination_field = generate_command('-sample.vfn ', False, input_name_discrimination_field, False)
+command_list.append(otb_input_name_discrimination_field)
+
+# Generate command for boost type
+otb_input_boost_type = generate_command('-classifier.boost.t ', False, input_boost_type, False)
+command_list.append(otb_input_boost_type)
+
+# Generate command for weak count
+otb_input_weak_count = generate_command('-classifier.boost.w ', False, input_weak_count, False)
+command_list.append(otb_input_weak_count)
+
+# Generate command for weight trim rate
+otb_input_weight_trim_rate = generate_command('classifier.boost.r ', False, input_weight_trim_rate, False)
+command_list.append(otb_input_weight_trim_rate)
+
+# Generate command for max depth tree
+otb_input_max_depth_tree = generate_command('classifier.boost.m ', False, input_max_depth_tree, False)
+command_list.append(otb_input_max_depth_tree)
+
+# Generate command for available RAM
+otb_input_ram = generate_command('-ram ', False, input_ram, False)
+command_list.append(otb_input_ram)
+
+# Generate command for output confusion matrix
+if len(out_conf_matrix) > 0:
+    otb_out_conf_matrix = generate_command('-io.confmatout ', True, out_conf_matrix, False)
+    command_list.append(otb_out_conf_matrix)
+
+# Generate command for output model file
+otb_out_model = generate_command('-io.out ', True, out_model, False)
+command_list.append(otb_out_model)
+
+# Generate full command for OTB
+otb_write_output = execute_command('otbcli_TrainImagesClassifier ', command_list, workspace, otb_dir)
+
+# Save command to log
+with open(log_file, 'w') as f:
+    f.write('Train Image Classifier (Bayes) Log')
+    f.write('\nTimestamp: {}'.format(ts))
+    f.write('\nInput Images: {}'.format(input_images.split(';')))
+    f.write('\nTrain/Validation Sample Ratio: {}'.format(input_train_validation_ratio))
+    f.write('\nBound sample number minimum: {}'.format(input_bound_sample_num_min))
+    f.write('\nDefault Elevation: {}'.format(input_default_elev))
+    f.write('\nBoost Type: {}'.format(input_boost_type))
+    f.write('\nWeak Count: {}'.format(input_weak_count))
+    f.write('\nWeight Trim Rate: {}'.format(input_weight_trim_rate))
+    f.write('\nMaximum Depth of Tree: {}'.format(input_max_depth_tree))
+    f.write('\nOutput Confusion Matrix: {}'.format(out_conf_matrix))
+    f.write('\nOutput Model: {}'.format(out_model))
+    f.write('\nOTB Command: {}'.format(otb_write_output))
